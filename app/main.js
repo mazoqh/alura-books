0544d329f5f368ec0fe98e18ab6c4a72c867229e
patrick <pzanatta37@gmail.com>
2024-06-03 22:57:54 -0300

    aula 2-5

--- app/main.js
+++ app/main.js
@@ -2,29 +2,10 @@ let livros = [];
 const endpointAPI =
   "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 getBuscarLivrosAPI();
-const elementoParaInserirLivros = document.getElementById("livros");
 
 async function getBuscarLivrosAPI() {
   const res = await fetch(endpointAPI);
   livros = await res.json();
-  console.table(livros);
-  exibirLivrosNaTela(livros);
-}
-
-function exibirLivrosNaTela(listaDeLivros) {
-  listaDeLivros.forEach((livro) => {
-    elementoParaInserirLivros.innerHTML += `
-    <div class="livro">
-    <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-    <h2 class="livro__titulo">
-      ${livro.titulo}
-    </h2>
-    <p class="livro__descricao">${livro.autor}</p>
-    <p class="livro__preco" id="preco">${livro.preco}</p>
-    <div class="tags">
-      <span class="tag">${livro.categoria}</span>
-    </div>
-  </div>
-  `;
-  });
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirLivrosNaTela(livrosComDesconto);
 }
---
+++ app/metodoFilter.js
@@ -0,0 +1,32 @@
+const botoes = document.querySelectorAll(".btn");
+botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
+
+function filtrarLivros() {
+  const elementoBtn = document.getElementById(this.id);
+  const categoria = elementoBtn.value;
+  let livrosFiltrados =
+    categoria == "disponivel"
+      ? filtrarPorDisponibilidade()
+      : filtrarPorCategoria(categoria);
+  exibirLivrosNaTela(livrosFiltrados);
+  if (categoria == "disponivel") {
+    const valorTotal = calculcarValorTotalDeLivrosDisponiveis(livrosFiltrados);
+    exibirValorTotalDosLivrosDiponiveisNaTela(valorTotal);
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDiponiveisNaTela(valorTotal) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+  <div class="livros__disponiveis">
+  <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+  </div>
+  `;
+}
